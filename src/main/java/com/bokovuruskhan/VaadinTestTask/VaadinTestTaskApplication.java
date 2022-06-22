bb46b129b4e4f408c418dbb6e6fee2ab7f953eca
Uruskhan Bokov <bokovuruskhan@gmail.com>
2022-06-22 13:57:42 +0300

    init commit

---
+++ src/main/java/com/bokovuruskhan/VaadinTestTask/VaadinTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.bokovuruskhan.VaadinTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bokovuruskhan/VaadinTestTask/VaadinTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bokovuruskhan.VaadinTestTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
